d9ed01043a4aa3b0acca297284dde752215b3f27
frubesss <robertson.cra@gmail.com>
2020-09-30 21:01:29 +0100

    sort out middleware

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,40 +1,38 @@
 const { Octokit } = require("@octokit/rest");
 const express = require("express");
 
-const app = express();
-const port = 3000;
-
-app.get("/githubsearch", async (req, res) => {
-  try {
-    const octokit = new Octokit({ auth: process.env.GITHUB_SEARCH_API_TOKEN });
-    const encodedComponentImportPath = encodeURI(req.query.componentImportPath);
-    octokit
-      .paginate(
-        `GET /search/code?q=${encodedComponentImportPath}`,
-        {
-          per_page: 100,
-        }
-      )
-      .then((responseJson) => {
-        const uniqueRepositories = filterUniqueRepositories(responseJson);
-        const formatResponse = uniqueRepositories.map((item) => {
-          return {
-            repositoryName: item.repository.name,
-            repositoryUrl: item.repository.html_url,
-            itemSha: item.sha,
-          };
-        });
-        res.json(formatResponse);
-      })
-      .catch((error) => {
-        res.status(500).json({ error: error.toString() });
+const expressMiddleWare = (router) => {
+  router.get("/githubsearch", async (req, res) => {
+    try {
+      const octokit = new Octokit({
+        auth: process.env.GITHUB_SEARCH_API_TOKEN,
       });
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-  }
-});
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+      const encodedComponentImportPath = encodeURI(
+        req.query.componentImportPath
+      );
+      octokit
+        .paginate(`GET /search/code?q=${encodedComponentImportPath}`, {
+          per_page: 100,
+        })
+        .then((responseJson) => {
+          const uniqueRepositories = filterUniqueRepositories(responseJson);
+          const formatResponse = uniqueRepositories.map((item) => {
+            return {
+              repositoryName: item.repository.name,
+              repositoryUrl: item.repository.html_url,
+              itemSha: item.sha,
+            };
+          });
+          res.json(formatResponse);
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.toString() });
+        });
+    } catch (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+  });
+};
 
 const filterUniqueRepositories = (responseJson) => {
   return [
